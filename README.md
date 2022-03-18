208891b8b13c6b5afaab224dd6db4c0edaa143bb
Tue Hellstern <tue@officegeek.dk>
2022-03-18 17:30:12 +0100

    Div

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Newscatcher
 Demo of the Newscatcher API
 
-## Links
+    Search multi-language worldwide news articles published online with NewsCatcher's News API
+
+In this demo I'm getting news articles regarding **Python and Excel**
+
+![](./image/news_1.jpg)
+
+# Files
+- newsapi.py
+- requirements.txt
+- extracted_news_articles.csv
+
+# How to run
+
+# Links
 - [newscatcherapi.com](https://newscatcherapi.com/)
 - [github.com/NewscatcherAPI/newscatcherapi-sdk-python](https://github.com/NewscatcherAPI/newscatcherapi-sdk-python)
 - [github.com/TueHellsternKea/newsapi](https://github.com/TueHellsternKea/newsapi/blob/main/README.md)
--- newsapi.py
+++ newsapi.py
@@ -1,11 +1,54 @@
-# Import
-from newscatcher import Newscatcher
+# Import packages
+# Default packages
+import time
+import csv
+import os
+import json
 
-# URL
-news_source = Newscatcher('nytimes.com')
-last_news_list = news_source.news
+# Preinstalled packages
+import requests
+import pandas as pd
 
-# Article attributes
-article = last_news_list[0]
-article.keys()
-#dict_keys(['title', 'title_detail', 'links', 'link', 'id', 'guidislink', 'summary', 'summary_detail', 'published', 'published_parsed', 'tags', 'media_content', 'media_credit', 'credit'])
+# Folder to save your .csv files
+# Windows
+os.chdir('C:\\Users\\tuehe\\Documents\\GitHub\\newsapi')
+
+# macOS
+#os.chdir('/Users/user_name/newsapi')
+
+# URL of our News API
+base_url = 'https://api.newscatcherapi.com/v2/search'
+
+# Your API key
+X_API_KEY = 'o1dOC6_gsS3rCsXT5sSyAf0cS4LCaRpOvx7lUJDu7wY'
+
+# Make an API call
+headers = {'x-api-key': X_API_KEY}
+
+# Define your desired parameters
+params = {
+    'q': 'Python and Excel',
+    'lang': 'en',
+    'to_rank': 10000,
+    'page_size': 100,
+    'page': 1
+    }
+
+# Make a call with both headers and params
+response = requests.get(base_url, headers=headers, params=params)
+
+# Encode received results
+results = json.loads(response.text.encode())
+if response.status_code == 200:
+    print('Done')
+else:
+    print(results)
+    print('ERROR: API call failed.')
+
+# Import data into pandas
+pandas_table = pd.DataFrame(results['articles'])
+
+#print(pandas_table)
+
+# Generate CSV from Pandas table
+pandas_table.to_csv('extracted_news_articles.csv', encoding='utf-8', sep=';')
